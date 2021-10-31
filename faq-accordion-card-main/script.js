067124d88e0399acccefa0041ac80d0ba1614a10
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-10-31 16:28:09 +0400

    FAQ accordion card 🚀

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,31 @@
+const questions = document.querySelectorAll(".question-div");
+const answers = document.querySelectorAll(".answer");
+const listElemenets = document.querySelectorAll("li");
+const arrows = document.querySelectorAll(".arrows");
+const mobileImg = document.querySelector(".mobile-img");
+const mobileBg = document.querySelector(".mobile-bg");
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", () => {
+    if (answers[i].classList.contains("clicked")) {
+      answers[i].classList.add("hide");
+      answers[i].classList.remove("clicked");
+      listElemenets[i].classList.remove("bold");
+      arrows[i].classList.remove("rotate");
+    } else {
+      answers[i].classList.remove("hide");
+      answers[i].classList.add("clicked");
+      listElemenets[i].classList.add("bold");
+      arrows[i].classList.add("rotate");
+    }
+  });
+}
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 920) {
+    mobileImg.classList.remove("hide");
+    mobileBg.classList.remove("hide");
+  } else {
+    mobileImg.classList.add("hide");
+    mobileBg.classList.add("hide");
+  }
+});
