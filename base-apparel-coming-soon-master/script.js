af6aded28a11dfd0a1b95a2f2b4c889a4953b0ec
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-11-06 19:47:23 +0400

    add email validation

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,5 +1,24 @@
 const button = document.querySelector("button");
+const email = document.querySelector("input");
+const valid = document.querySelector(".valid");
+const invalid = document.querySelector(".invalid");
+const errorLogo = document.querySelector(".error-img");
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(e);
+  if (validateEmail(email.value)) {
+    errorLogo.classList.add("hide");
+    invalid.classList.add("hide");
+    valid.classList.remove("hide");
+  } else {
+    errorLogo.classList.remove("hide");
+    valid.classList.add("hide");
+    invalid.classList.remove("hide");
+  }
 });
+
+function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
