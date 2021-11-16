9c7f7cca6687afb2cca8e033a9d820bfe413c5ee
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-11-16 11:34:01 +0400

    add error state

--- tip-calculator-app-main/public/script.js
+++ tip-calculator-app-main/public/script.js
@@ -1,26 +1,17 @@
 const tipButtons = document.querySelectorAll(".grid-item>button");
 const bill = document.querySelector(".bill");
 const people = document.querySelector(".people");
-let tipAmount = document.querySelector(".tip-value");
-let totalAmount = document.querySelector(".total-value");
+const tipAmount = document.querySelector(".tip-value");
+const totalAmount = document.querySelector(".total-value");
+const resetBtn = document.querySelector(".reset");
+const error = document.querySelector(".error");
 
 function tipValues() {
   tipButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
+      console.log(parseInt(btn.textContent));
       return parseInt(btn.textContent);
     });
-  });
-}
-
-function billValue() {
-  bill.addEventListener("input", (num) => {
-    calculateBill(num.target.value);
-  });
-}
-
-function peopleValue() {
-  people.addEventListener("input", (amount) => {
-    calculateTotal(amount.target.value);
   });
 }
 
@@ -32,5 +23,43 @@ function calculateTotal(amount) {
   totalAmount.textContent = `\$${amount}`;
 }
 
+function billValue() {
+  bill.addEventListener("input", (num) => {
+    calculateBill(num.target.value);
+  });
+}
+
+function peopleValue() {
+  let zeroAmount = 0;
+  people.addEventListener("input", (amount) => {
+    if (amount.target.value == "0") {
+      addError();
+      amount.preventDefault();
+    } else {
+      removeError();
+      calculateTotal(amount.target.value);
+    }
+  });
+}
+
+function addError() {
+  people.parentNode.classList.remove("hover-green");
+  people.parentNode.classList.add("hover-red");
+  error.classList.remove("hide");
+}
+function removeError() {
+  people.parentNode.classList.remove("hover-red");
+  people.parentNode.classList.add("hover-green");
+  error.classList.add("hide");
+}
+
+function reset() {
+  tipAmount.textContent = "$0.00";
+  totalAmount.textContent = "$0.00";
+}
+
 billValue();
 peopleValue();
+resetBtn.addEventListener("click", () => {
+  reset();
+});
