03c7f1d7b04a6209389329c5a4a3b89551a26ad2
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-11-23 21:45:12 +0400

    add tip value function

--- tip-calculator-app-main/public/script.js
+++ tip-calculator-app-main/public/script.js
@@ -5,53 +5,37 @@ const tipAmount = document.querySelector(".tip-value");
 const totalAmount = document.querySelector(".total-value");
 const resetBtn = document.querySelector(".reset");
 const error = document.querySelector(".error");
+const calculateBtn = document.querySelector(".calcBtn");
 
-let tip = 0,
-  amt = 0,
-  ppl = 1;
+tip = 0;
 
 tipButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (btn.classList.contains("active")) {
-      tip = 0;
       btn.classList.remove("active");
-      btn.classList.add("unactive");
+      btn.classList.add("inactive");
     } else {
       tipButtons.forEach((e) => {
         e.classList.remove("active");
       });
-      tip = btn.value;
-      btn.classList.remove("unactive");
+      tip = parseInt(btn.textContent);
+      btn.classList.remove("inactive");
       btn.classList.add("active");
     }
   });
 });
 
-function calculateBill(bill) {
-  tipAmount.textContent = `\$${bill}`;
-  console.log((bill * tipValue()) / 100 / people);
+function calculateBill() {
+  if (bill.value && people.value && tip) {
+    let res = (bill.value * tip) / 100 / people.value;
+    tipAmount.textContent = `\$${res}`;
+  } else {
+    alert("Fill Fields");
+  }
 }
 
 function calculateTotal(amount) {
   totalAmount.textContent = `\$${amount}`;
-}
-
-function billValue() {
-  bill.addEventListener("input", (bill) => {
-    calculateBill(bill.target.value);
-  });
-}
-
-function peopleValue() {
-  people.addEventListener("input", (amount) => {
-    if (amount.target.value == "0") {
-      addError();
-      amount.preventDefault();
-    } else {
-      removeError();
-      calculateTotal(amount.target.value);
-    }
-  });
 }
 
 function addError() {
@@ -72,4 +56,8 @@ function reset() {
 
 resetBtn.addEventListener("click", () => {
   reset();
+});
+
+calculateBtn.addEventListener("click", () => {
+  calculateBill();
 });
