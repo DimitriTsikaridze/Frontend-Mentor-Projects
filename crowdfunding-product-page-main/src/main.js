bf2e121ad523a483d9a7635bc4b75fab7cc5b8d5
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-12-17 20:12:38 +0400

    finish project without js logic

--- crowdfunding-product-page-main/src/main.js
+++ crowdfunding-product-page-main/src/main.js
@@ -1,7 +1,7 @@
-const backProjectBtn = document.querySelector(".back-project-btn");
-const backProjectModal = document.querySelector(".section-1-modal");
-const overlay = document.querySelector(".overlay");
-backProjectBtn.addEventListener("click", () => {
-  overlay.classList.remove("hide");
-  backProjectModal.classList.remove("hide");
-});
+// const backProjectBtn = document.querySelector(".back-project-btn");
+// const backProjectModal = document.querySelector(".section-1-modal");
+// const overlay = document.querySelector(".overlay");
+// backProjectBtn.addEventListener("click", () => {
+//   overlay.classList.remove("hide");
+//   backProjectModal.classList.remove("hide");
+// });
