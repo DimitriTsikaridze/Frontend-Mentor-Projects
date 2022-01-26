e67e28d06c5a749c54d6d1782c3b7d4e0b5dd6ca
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-26 21:53:45 +0400

    remove comments

--- crowdfunding-product-page-main/src/main.js
+++
@@ -1,7 +0,0 @@
-// const backProjectBtn = document.querySelector(".back-project-btn");
-// const backProjectModal = document.querySelector(".section-1-modal");
-// const overlay = document.querySelector(".overlay");
-// backProjectBtn.addEventListener("click", () => {
-//   overlay.classList.remove("hide");
-//   backProjectModal.classList.remove("hide");
-// });
