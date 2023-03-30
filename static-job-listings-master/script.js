380b09407f65e6c1382e20a5828f7e74e4f1fb35
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-03-30 13:13:43 -0700

    add job styles

--- static-job-listings-master/script.js
+++ static-job-listings-master/script.js
@@ -1,9 +1,12 @@
 const jobsContainer = document.querySelector(".jobs-container")
 
+const searchTerms = []
+
 const loadJobs = async () => {
   const jobs = await (await fetch("data.json")).json()
   jobs.forEach((job) => {
     const article = document.createElement("article")
+    job.featured ? article.classList.add("featured-job") : null
     const div = document.createElement("div")
     job.languages.forEach((lang) => {
       const span = document.createElement("span")
@@ -15,8 +18,8 @@ const loadJobs = async () => {
       <img src="${job.logo}" alt="logo image"/>
       <div>
         <h3>${job.company} 
-        ${job.new ? "<span>NEW!</span>" : ""} 
-        ${job.new ? "<span>FEATURED!</span>" : ""} 
+        ${job.new ? "<span class='new'>NEW!</span>" : ""} 
+        ${job.featured ? "<span class='featured'>FEATURED</span>" : ""} 
         </h3>
         <h2>${job.position}</h2>
         <h4>
@@ -29,6 +32,7 @@ const loadJobs = async () => {
     article.appendChild(div)
     jobsContainer.appendChild(article)
   })
+  const languages = document.querySelectorAll(".languages")
 }
 
 loadJobs()
