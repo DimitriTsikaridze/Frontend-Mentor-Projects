b6f020498a9499ec4b7c6e50e3c5e086ff1d8046
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-05-11 00:46:13 -0700

    finish desktop

--- age-calculator-app-main/public/app.js
+++ age-calculator-app-main/public/app.js
@@ -3,8 +3,27 @@ const day = document.querySelector(".day")
 const month = document.querySelector(".month")
 const year = document.querySelector(".year")
 
+const outputYears = document.querySelector(".years")
+const outputMonths = document.querySelector(".months")
+const outputdays = document.querySelector(".days")
+
 submitBtn.onclick = () => {
-  console.log(!day.value, !month.value, !year.value)
+  if (!isEmpty() && isValidYear() && isValidMonth() && isValidDay()) {
+    document
+      .querySelectorAll("input")
+      .forEach((el) => el.classList.remove("error"))
+    const { years, months, days } = calculateCurrentAge(
+      day.value,
+      month.value,
+      year.value
+    )
+    outputYears.textContent = years
+    outputMonths.textContent = months
+    outputdays.textContent = days
+  } else {
+    day.nextElementSibling.textContent = "Must be a valid date"
+    day.classList.add("error")
+  }
 }
 
 const isEmpty = () => {
@@ -21,6 +40,9 @@ const isEmpty = () => {
   if (!year.value) {
     formatControl(year)
   }
+  if (day.value && month.value && year.value) {
+    return false
+  }
   return true
 }
 
@@ -28,23 +50,26 @@ const isValidYear = () => {
   if (year.value > new Date().getFullYear()) {
     year.classList.add("error")
     year.nextElementSibling.textContent = "Must be in past"
+    return false
   }
+  return true
 }
 
 const isValidMonth = () => {
-  if (month.value > 12 || month.value === 0) {
+  if (month.value > 12 || month.value == 0) {
     month.classList.add("invalid")
+    return false
   }
+  return true
 }
 
 const isValidDay = () => {
   const getDaysInMonth = (year, month) => {
     return new Date(year, month, 0).getDate()
   }
-  if (day.value === 0 || day.value > getDaysInMonth(year.value, month.value)) {
-    console.log("invalid")
+  if (day.value == 0 || day.value > getDaysInMonth(year.value, month.value)) {
     day.classList.add("invalid")
-    return
+    return false
   }
 
   return true
@@ -52,16 +77,10 @@ const isValidDay = () => {
 
 const calculateCurrentAge = (birthDay, birthMonth, birthYear) => {
   const today = new Date()
-  const birthDate = new Date(birthYear, birthMonth - 1, birthDay)
-  let age = today.getFullYear() - birthDate.getFullYear()
-  const month = today.getMonth() - birthDate.getMonth()
-  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-    age--
-  }
-  const year = age
-  const day = Math.floor(
-    (today.getTime() - birthDate.getTime()) / (1000 * 60 * 60 * 24)
-  )
-  const months = month < 0 ? month + 12 : month
-  return { year, months, day }
+  const birthdate = new Date(birthYear, birthMonth - 1, birthDay)
+  const ageDelta = today - birthdate
+  const years = Math.floor(ageDelta / 1000 / 60 / 60 / 24 / 365)
+  const months = Math.floor(ageDelta / 1000 / 60 / 60 / 24 / 30) % 12
+  const days = Math.floor(ageDelta / 1000 / 60 / 60 / 24) % 30
+  return { years, months, days }
 }
