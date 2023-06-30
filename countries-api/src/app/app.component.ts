decc95f71a0ee29a12a0682398ac8a7589f63d7c
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-06-29 23:58:14 -0700

    add reset styles

--- countries-api/src/app/app.component.ts
+++ countries-api/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'countries-api';
+  title = "countries-api"
 }
