5d606cbd124dc530025cab9b17f8fe6483476134
DimitriTsikaridze <dtsikaridze10@proton.me>
2024-10-14 17:21:24 +0400

    add your cart

--- ecommerce-product-page-main/src/app/header/header.component.ts
+++ ecommerce-product-page-main/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { YourCartComponent } from "../your-cart/your-cart.component";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [],
+  imports: [YourCartComponent],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
