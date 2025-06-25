4890d1dadbe618f52d6de97b4ef07cb1ef50696d
DimitriTsikaridze <dtsikaridze@proton.me>
2025-06-25 18:21:39 +0400

    add zod and filtering functionality

--- browser-extensions-manager-ui-main/src/app/app.config.ts
+++ browser-extensions-manager-ui-main/src/app/app.config.ts
@@ -1,17 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
   ],
 };
--- browser-extensions-manager-ui-main/src/app/app.ts
+++ browser-extensions-manager-ui-main/src/app/app.ts
@@ -1,13 +1,83 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { httpResource } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import * as z from 'zod/v4-mini';
+
+const Extensions = z.array(
+  z.object({
+    logo: z.string(),
+    name: z.string(),
+    description: z.string(),
+    isActive: z.boolean(),
+  })
+);
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgOptimizedImage],
   template: `
-    <h1 class="text-3xl text-green-500">Welcome to {{ title }}!</h1>
+    <header class="flex justify-between bg-neutral-700">
+      <img width="179" height="41" ngSrc="images/logo.svg" priority alt="Logo" />
+      <button>
+        <img width="22" height="22" ngSrc="images/icon-sun.svg" alt="Light Mode" />
+      </button>
+      <!-- <button >
+        <img width="22" height="22" ngSrc="images/icon-moon.svg" alt="Dark Mode" />
+      </button> -->
+    </header>
+    <main>
+      <div class="flex justify-between">
+        <h1>Extensions List</h1>
+        <div class="flex gap-2">
+          <button class="cursor-pointer" (click)="query.set('all')">All</button>
+          <button class="cursor-pointer" (click)="query.set('active')">Active</button>
+          <button class="cursor-pointer" (click)="query.set('inactive')">Inactive</button>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-3 gap-2">
+        @for (extension of filteredExtensions(); track extension.name) {
+        <div>
+          <img width="60" height="60" [ngSrc]="extension.logo" [alt]="extension.name" />
+          <p>{{ extension.name }}</p>
+          <p>{{ extension.description }}</p>
+
+          <div class="flex justify-between">
+            <button>Remove</button>
+            <input [checked]="extension.isActive" type="checkbox" />
+          </div>
+        </div>
+        }
+      </div>
+    </main>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected title = 'browser-extension-manager';
+  query = signal<Query>('all');
+  extensionsResource = httpResource(() => 'data.json', {
+    defaultValue: [],
+    parse: Extensions.parse,
+  });
+  filteredExtensions = computed(() => {
+    const extensions = this.extensionsResource.value();
+    const query = this.query();
+    switch (query) {
+      case 'active':
+        return extensions.filter(({ isActive }) => isActive);
+      case 'inactive':
+        return extensions.filter(({ isActive }) => !isActive);
+      default:
+        return extensions;
+    }
+  });
 }
+
+export interface Extension {
+  logo: string;
+  name: string;
+  description: string;
+  isActive: boolean;
+}
+
+export type Query = 'all' | 'active' | 'inactive';
