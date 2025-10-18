bdcab742970f9e556e9dc82fc014d48b5c544756
DimitriTsikaridze <dtsikaridze@proton.me>
2025-10-18 16:41:14 +0400

    add styling

--- weather-app-main/src/app/app.ts
+++ weather-app-main/src/app/app.ts
@@ -1,11 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected readonly title = signal('weather-app-main');
+  stats = signal<{ title: string; value: string }[]>([
+    { title: 'Feels Like', value: '18 °' },
+    { title: 'Humidity', value: '46%' },
+    { title: 'Wind', value: '144 km/h' },
+    { title: 'Precipitation', value: '0 mm' }
+  ]);
+
+  dailyForecast = signal<{ day: string; maxTemp: string; minTemp: string; image: string }[]>([
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' },
+    { image: '/public/images/icon-overcast.webp', day: 'Tue', minTemp: '14°', maxTemp: '20°' }
+  ]);
+
+  hourlyForecast = signal<{ image: string; hour: string; temp: string }[]>([
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '3pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '4pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '5pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '6pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '7pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '8pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '9pm', temp: '20°' },
+    { image: '/public/images/icon-partly-cloudy.webp', hour: '10pm', temp: '20°' }
+  ]);
 }
