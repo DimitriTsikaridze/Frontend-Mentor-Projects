fa42f4a598d4f0588a901bd9050d8923b74aba4d
DimitriTsikaridze <dtsikaridze@proton.me>
2025-10-18 22:09:07 +0400

    add units dropdown

--- weather-app-main/src/app/header/header.ts
+++ weather-app-main/src/app/header/header.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Header {
+  units = signal([
+    { title: 'Temperature', options: ['Celsius (°C)', 'Fahrenheit (°F)'] },
+    { title: 'Wind Speed', options: ['km/h', 'mph'] },
+    { title: 'Precipitation', options: ['Millimeters (mm)', 'Inches (in)'] }
+  ]);
 
+  showUnits = signal(false);
+  isImperial = signal(false);
 }
